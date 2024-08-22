8f653971a734c6714f739ac55576f7b04287f731
alejandrossm <alejandrossm@outlook.com>
2024-08-22 09:40:39 -0400

    Home listo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'fundamentos_angular_s2';
+  title = 'Fundamentos Angular Sección 2';
 }
